7a2b954f984ced3cf8d5ee39a8ed8f508671ac99
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-06-08 23:57:28 -0700

    Created New React and Facts and Nav Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Fun from './facts';
+
+function App() {
+  return (
+    <div className="App">
+      <App />
+      <Fun />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Nav(){
+
+    return(
+
+        <div class="nav">
+            <img  style={{width: 50}} src="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"></img>
+            <h3>React Facts</h3>
+            <span>React Course - Project 1</span>
+      </div>
+
+
+
+    )
+
+
+
+}
